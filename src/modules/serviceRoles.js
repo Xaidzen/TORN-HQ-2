403cb14473d878baca4e13b230d94923c82ee560
Xaidzen <uzumakinaruto032812@gmail.com>
2026-09-03 22:53:47 +0900

    Add service roles management functionality

--- src/modules/serviceRoles.js
+++ src/modules/serviceRoles.js
@@ -1,1 +1,24 @@
+const config = require("../utils/config");
 
+const SERVICE_ROLES = {
+    service_loss: config.LOSS_SELLER_ROLE_ID,
+    service_escape: config.ESCAPE_SELLER_ROLE_ID,
+    service_bounty: config.BOUNTY_PLACER_ROLE_ID,
+    service_detective: config.AGENCY_DETECTIVE_ROLE_ID
+};
+
+async function giveServiceRole(member, buttonId) {
+    const roleId = SERVICE_ROLES[buttonId];
+
+    if (!roleId) {
+        return false;
+    }
+
+    await member.roles.add(roleId);
+
+    return true;
+}
+
+module.exports = {
+    giveServiceRole
+};
