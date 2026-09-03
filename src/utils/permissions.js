ec42802b6838f5f170071869109d9a8247e9cdab
Xaidzen <uzumakinarutop32812@gmail.com>
2026-09-04 01:58:20 +0900

    Fix bot permissions and project files

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,1 +1,27 @@
+const config = require("./config");
 
+function isAdmin(member) {
+    return Boolean(
+        member &&
+        (
+            member.permissions.has("Administrator") ||
+            member.roles.cache.has(config.ADMIN_ROLE_ID)
+        )
+    );
+}
+
+function isStaff(member) {
+    return Boolean(
+        member &&
+        (
+            member.permissions.has("Administrator") ||
+            member.roles.cache.has(config.ADMIN_ROLE_ID) ||
+            member.roles.cache.has(config.STAFF_ROLE_ID)
+        )
+    );
+}
+
+module.exports = {
+    isAdmin,
+    isStaff
+};
